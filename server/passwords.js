6fdb4fa318afc14c5bd32490bc47a41065ce7d46
Arthur Quinn <arthurquinn92@gmail.com>
2021-01-27 22:56:05 -0500

    handle errors

--- server/passwords.js
+++ server/passwords.js
@@ -5,7 +5,13 @@ const saltRounds = 10;
 const updatePassword = intel => {
   return new Promise((resolve, reject) => {
     bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
       bcrypt.hash(intel.password, salt, async (err, hash) => {
+        if (err) {
+          reject(err);
+        }
         intel.password = hash;
         try {
           const updated = await queries.updatePassword(intel);
