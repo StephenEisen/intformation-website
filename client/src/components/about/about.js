30ea3cacb3405dfa4747d550b8bcd0a58dcee6bb
StephenEisen <stephen.eisen27@gmail.com>
2021-01-30 15:48:43 -0500

    added garbage placeholder text to about page

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -10,6 +10,21 @@ const About = () => {
       <div>
         Hello! My name is Steve and I'm a Software Developer! With the help of two good friends we developed this website from scratch.
       </div>
+
+      <p>
+        Want to help us out?
+      </p>
+      <div>
+        Running this website for all you tryhard weebs out there is a little costly. If you want to help out we would gladly accept any donation you would be willing it give us.
+        Any amount is helpful and goes to keeping the website active.
+      </div>
+
+      <p>
+        Feel free to look at other projects that we've worked on!
+      </p>
+      <div>
+        github garbage links here
+      </div>
     </div>
   );
 };
