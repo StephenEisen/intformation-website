c2c405fb2950375ba809c1ca5409331f06c57829
StephenEisen <stephen.eisen27@gmail.com>
2021-01-30 15:25:02 -0500

    added monke to about page KEKW

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -1,9 +1,15 @@
 import React from "react";
+import monke from "assets/monke1.png";
+import './about.css';
 
 const About = () => {
   return (
     <div className="container">
-      <p>About page</p>
+      <img className="monke" alt="monke" src={monke} />
+
+      <div>
+        Hello! My name is Steve and I'm a Software Developer! With the help of two good friends we developed this website from scratch.
+      </div>
     </div>
   );
 };
